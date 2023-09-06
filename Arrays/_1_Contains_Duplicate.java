ef5e82e61fa3d9d3ee55d3722317ca0083326b45
Gauravzz12 <gaurav1234thakurgt@gmail.com>
2023-09-06 21:27:05 +0530

    ContainsDuplicate

---
+++ Arrays/_1_Contains_Duplicate.java
@@ -0,0 +1,11 @@
+package Arrays;
+import java.util.*;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> set=new HashSet<>();
+        for(int i:nums){
+            set.add(i);
+        }
+        return nums.length>set.size();
+    }
+}
