5a9e666d6631d1c570974b8ec94e1bd9c97ddf90
Gauravzz12 <131347059+Gauravzz12@users.noreply.github.com>
2023-09-06 21:40:01 +0530

    Update _2_Valid_Anagram.java

--- Arrays/_2_Valid_Anagram.java
+++ Arrays/_2_Valid_Anagram.java
@@ -1,6 +1,6 @@
 package Arrays;
 
- class Solution1 {
+ class Solution {
     public boolean isAnagram(String s, String t) {
         int[] alphabet = new int[26];
         for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
