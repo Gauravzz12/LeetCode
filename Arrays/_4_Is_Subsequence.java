a922389260a9cd51ec4e4ebba139e1113e34b176
Gauravzz12 <gaurav1234thakurgt@gmail.com>
2023-09-08 19:34:01 +0530

    Is Subsequence

---
+++ Arrays/_4_Is_Subsequence.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isSubsequence(String s, String t) {
+        int S=s.length();
+        int T=t.length();
+        int i=0,j=0;
+        if(S<1)
+        return true;
+        if(S>T || T<1)
+        return false;
+        while(i<T){
+            if(s.charAt(j)==t.charAt(i))
+            j++;
+            i++;
+            if(j==S)
+            return true;
+        }
+        return false;
+    }
+}
