719f9e40a906b8fe6c16af4d9f765e7864aced85
Gauravzz12 <gaurav1234thakurgt@gmail.com>
2023-09-08 19:11:56 +0530

    Replace Elements with Greatest Element on Right Side

---
+++ Arrays/_4_Greatest_on_the_right.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int[] replaceElements(int[] arr) {
+        int n=arr.length;
+        int greatest=arr[n-1];
+        for(int i=n-1;i>=0;i--){
+            int x=arr[i];
+            arr[i]=greatest;
+            greatest=Math.max(greatest,x);
+        }
+        arr[n-1]=-1;
+        return arr;
+    }
+}
